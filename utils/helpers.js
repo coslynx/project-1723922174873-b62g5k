f2c12f94a4c51f5775fc6312c046bc379dfebbcf
Spectra.codes by Drix10 <drixisgod@gmail.com>
2024-08-18 00:52:33 +0530

    generated file: `utils/helpers.js`

---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const options = { month: 'long', day: 'numeric', year: 'numeric' };
+  return date.toLocaleDateString('en-US', options);
+};
+
+module.exports = {
+  formatDate,
+};
